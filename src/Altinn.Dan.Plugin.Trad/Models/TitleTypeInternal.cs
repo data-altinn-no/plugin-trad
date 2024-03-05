bb678fce5cb435bfdf52ccd6482726734d7cb1a8
erlend oksvoll <erlend.oksvoll@digdir.no>
2024-03-05 14:47:00 +0100

    fixed enumparsing of title

--- src/Altinn.Dan.Plugin.Trad/Models/TitleTypeInternal.cs
+++ src/Altinn.Dan.Plugin.Trad/Models/TitleTypeInternal.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json.Converters;
 
 namespace Altinn.Dan.Plugin.Trad.Models
 {
-    [JsonConverter(typeof(TitleTypeInternalStringEnumConverter))]
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum TitleTypeInternal
     {
         [EnumMember] 
@@ -17,7 +17,7 @@ namespace Altinn.Dan.Plugin.Trad.Models
         [EnumMember]
         Advokatfullmektig,
 
-        [EnumMember]
+        [EnumMember(Value="Rettshjelper nr1")]
         Rettshjelper,
 
         [EnumMember(Value = "EØS-Advokat")]
@@ -25,19 +25,5 @@ namespace Altinn.Dan.Plugin.Trad.Models
 
         [EnumMember(Value = "Utenlandsk advokat")]
         UtenlandskAdvokat
-    }
-
-    public class TitleTypeInternalStringEnumConverter : StringEnumConverter
-    {
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.Value == null || string.IsNullOrEmpty(reader.Value.ToString()) ||
-                !Enum.TryParse<TitleTypeInternal>(reader.Value.ToString(), true, out _))
-            {
-                return TitleTypeInternal.Ukjent;
-            }
-
-            return base.ReadJson(reader, objectType, existingValue, serializer);
-        }
     }
 }
