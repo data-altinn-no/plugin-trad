1dece4dd59214f5a29c47bed3f2dde4d2b08e71b
SondreJDigdir <sondre.jahrsengene@digdir.no>
2025-02-24 15:03:05 +0100

    feat: add caching for org names (#18)

---
+++ src/Altinn.Dan.Plugin.Trad/Services/OrganizationService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Dan.Common.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Altinn.Dan.Plugin.Trad.Services;
+
+public interface IOrganizationService
+{
+    Task<string> GetOrgName(int orgNr);
+}
+
+public class OrganizationService(IEntityRegistryService entityRegistryService, IDistributedCache cache)
+    : IOrganizationService
+{
+    private const string OrganizationCachePrefix = "trad-org";
+
+    public async Task<string> GetOrgName(int orgNr)
+    {
+        var key = GetCacheKey(orgNr);
+        var result = await cache.GetAsync(key);
+        if (result != null)
+        {
+            return Encoding.UTF8.GetString(result);
+        }
+
+        var practiceOrg = await entityRegistryService.GetFull(orgNr.ToString());
+        if (practiceOrg?.Navn is null) return null;
+
+        await cache.SetAsync(key, Encoding.UTF8.GetBytes(practiceOrg.Navn), new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24)
+        });
+        return practiceOrg.Navn;
+
+    }
+    
+
+    private static string GetCacheKey(int orgNr) => $"{OrganizationCachePrefix}{orgNr}";
+}
