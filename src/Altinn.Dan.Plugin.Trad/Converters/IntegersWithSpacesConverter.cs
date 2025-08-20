bdf83cacc455cb70a04b8e348ccee75ef4ed400b
Sondre Jahrsengene <sondre.jahrsengene@digdir.no>
2025-08-20 15:51:25 +0200

    feat: add converter for numbers with spaces in them

---
+++ src/Altinn.Dan.Plugin.Trad/Converters/IntegersWithSpacesConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Altinn.Dan.Plugin.Trad.Converters;
+
+public class IntegersWithSpacesConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        var token = JToken.FromObject(value);
+        token.WriteTo(writer);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        var value = reader.Value;
+        return value switch
+        {
+            null or int => value,
+            long l => (int)l,
+            string s => int.Parse(s.Replace(" ", string.Empty)),
+            _ => throw new JsonSerializationException($"Unable to serialize property {value} to integer")
+        };
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(int) || objectType == typeof(int?) || objectType == typeof(string);
+    }
+}
--- src/Altinn.Dan.Plugin.Trad/Models/PracticeInternal.cs
+++ src/Altinn.Dan.Plugin.Trad/Models/PracticeInternal.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Altinn.Dan.Plugin.Trad.Converters;
 using Newtonsoft.Json;
 
 namespace Altinn.Dan.Plugin.Trad.Models
@@ -9,9 +10,11 @@ namespace Altinn.Dan.Plugin.Trad.Models
         public string CompanyNumber { get; set; }
 
         [JsonProperty("orgNumber")]
+        [JsonConverter(typeof(IntegersWithSpacesConverter))]
         public int OrganizationNumber { get; set; }
 
         [JsonProperty("subOrgNumber", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(IntegersWithSpacesConverter))]
         public int? SubOrganizationNumber { get; set; }
 
         [JsonProperty("authorizedRepresentatives", NullValueHandling = NullValueHandling.Ignore)]
@@ -26,27 +29,5 @@ namespace Altinn.Dan.Plugin.Trad.Models
         // Not fetched from trad-registry, fetched from ER, so no json property needed, as we'll cache as we go
         public string OrganizationName { get; set; }
         public string SubOrganizationName { get; set; }
-    }
-    
-    // Temp model in lieu of a better fix
-    public record PracticeInternalTemp
-    {
-        [JsonProperty("firmanr", NullValueHandling = NullValueHandling.Ignore)]
-        public string CompanyNumber { get; set; }
-
-        [JsonProperty("orgNumber")]
-        public string OrganizationNumber { get; set; }
-
-        [JsonProperty("subOrgNumber", NullValueHandling = NullValueHandling.Ignore)]
-        public string SubOrganizationNumber { get; set; }
-
-        [JsonProperty("authorizedRepresentatives", NullValueHandling = NullValueHandling.Ignore)]
-        public List<PersonInternalTemp> AuthorizedRepresentatives;
-
-        [JsonProperty("isAuthorizedRepresentativeFor", NullValueHandling = NullValueHandling.Ignore)]
-        public List<PersonInternalTemp> IsAnAuthorizedRepresentativeFor;
-        
-        [JsonProperty("hovedpraksis", NullValueHandling = NullValueHandling.Ignore)]
-        public bool? MainPractice;
     }
 }
