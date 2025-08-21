ba08cec78146d3cd8f9b38617e18c91a99f067bf
SondreJDigdir <sondre.jahrsengene@digdir.no>
2025-08-21 11:46:48 +0200

    feat: add converter for numbers with spaces in them (#33)

--- src/Altinn.Dan.Plugin.Trad/Models/PersonInternal.cs
+++ src/Altinn.Dan.Plugin.Trad/Models/PersonInternal.cs
@@ -26,29 +26,4 @@ namespace Altinn.Dan.Plugin.Trad.Models
         [JsonProperty("practices", NullValueHandling = NullValueHandling.Ignore)]
         public List<PracticeInternal> Practices { get; set; }
     }
-    
-    // Temp model in lieu of a better fix
-    public record PersonInternalTemp
-    {
-        [JsonProperty("regnr", NullValueHandling = NullValueHandling.Ignore)]
-        public string RegistrationNumber { get; set; }
-
-        [JsonProperty("ssn")]
-        public string Ssn { get; set; }
-        
-        [JsonProperty("title")]
-        public TitleTypeInternal Title { get; set; }
-        
-        [JsonProperty("firstName")]
-        public string Firstname { get; set; }
-        
-        [JsonProperty("middleName")]
-        public string MiddleName { get; set; }
-        
-        [JsonProperty("lastName")]
-        public string LastName { get; set; }
-
-        [JsonProperty("practices", NullValueHandling = NullValueHandling.Ignore)]
-        public List<PracticeInternalTemp> Practices { get; set; }
-    }
 }
